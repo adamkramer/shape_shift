77b8b754cb0a97b9630962a4a733736a1c014b05
adamkramer <adamkramer@hotmail.com>
2017-10-22 21:40:44 +0100

    Update shape_shift.py

--- shape_shift.py
+++ shape_shift.py
@@ -7,6 +7,7 @@ import difflib
 # List of countries which we want appear to be coming from - 
 # Add/remove per your requirements - second field is ISO_3166 country code 
 # Country codes can be obtained from: https://en.wikipedia.org/wiki/ISO_3166-1#Current_codes
+# If you have preferred proxy for a particular country, place it in the 3rd field in format IP:port and it will be used
 
 proxylist_matrix = [
 					["United States", "US", ""],
@@ -36,27 +37,27 @@ proxylist_matrix = [
 # Add / remove per your requirements - first field is friendly name, second field is user-agent you want to send
 
 useragent_matrix = [
-					["Google Chrome", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", ""],
-					["Mozilla Firefox","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",""],
-					["Microsoft Edge","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",""],
-					["Microsoft Internet Explorer 6 / IE 6","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",""],
-					["Microsoft Internet Explorer 7 / IE 7","Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",""],
-					["Microsoft Internet Explorer 8 / IE 8","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)",""],
-					["Microsoft Internet Explorer 9 / IE 9","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0;  Trident/5.0)",""],
-					["Microsoft Internet Explorer 10 / IE 10","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; MDDCJS)",""],
-					["Microsoft Internet Explorer 11 / IE 11","Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",""],
-					["Apple iPad","Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",""],
-					["Apple iPhone","Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",""],
-					["Googlebot (Google Search Engine Bot)","Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",""],
-					["Bing Bot (Bing Search Engine Bot)","Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",""],
-					["Samsung Phone","Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G570Y Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36",""],
-					["Samsung Galaxy Note 3","Mozilla/5.0 (Linux; Android 5.0; SAMSUNG SM-N900 Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/2.1 Chrome/34.0.1847.76 Mobile Safari/537.36",""],
-					["Samsung Galaxy Note 4","Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-N910F Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36",""],
-					["Google Nexus","Mozilla/5.0 (Linux; U; Android-4.0.3; en-us; Galaxy Nexus Build/IML74K) AppleWebKit/535.7 (KHTML, like Gecko) CrMo/16.0.912.75 Mobile Safari/535.7",""],
-					["HTC","Mozilla/5.0 (Linux; Android 7.0; HTC 10 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.83 Mobile Safari/537.36",""],
-					["Curl","curl/7.35.0",""],
-					["Wget","Wget/1.15 (linux-gnu)",""],
-					["Lynx","Lynx/2.8.8pre.4 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.12.23",""]
+					["Google Chrome", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"],
+					["Mozilla Firefox","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"],
+					["Microsoft Edge","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"],
+					["Microsoft Internet Explorer 6 / IE 6","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"],
+					["Microsoft Internet Explorer 7 / IE 7","Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)"],
+					["Microsoft Internet Explorer 8 / IE 8","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)"],
+					["Microsoft Internet Explorer 9 / IE 9","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0;  Trident/5.0)"],
+					["Microsoft Internet Explorer 10 / IE 10","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; MDDCJS)"],
+					["Microsoft Internet Explorer 11 / IE 11","Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko"],
+					["Apple iPad","Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4"],
+					["Apple iPhone","Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"],
+					["Googlebot (Google Search Engine Bot)","Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"],
+					["Bing Bot (Bing Search Engine Bot)","Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)"],
+					["Samsung Phone","Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G570Y Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36"],
+					["Samsung Galaxy Note 3","Mozilla/5.0 (Linux; Android 5.0; SAMSUNG SM-N900 Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/2.1 Chrome/34.0.1847.76 Mobile Safari/537.36"],
+					["Samsung Galaxy Note 4","Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-N910F Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36"],
+					["Google Nexus","Mozilla/5.0 (Linux; U; Android-4.0.3; en-us; Galaxy Nexus Build/IML74K) AppleWebKit/535.7 (KHTML, like Gecko) CrMo/16.0.912.75 Mobile Safari/535.7"],
+					["HTC","Mozilla/5.0 (Linux; Android 7.0; HTC 10 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.83 Mobile Safari/537.36"],
+					["Curl","curl/7.35.0"],
+					["Wget","Wget/1.15 (linux-gnu)"],
+					["Lynx","Lynx/2.8.8pre.4 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.12.23"]
 					]
 
 # Shameless welcome banner					
@@ -173,21 +174,21 @@ for i in range(len(proxylist_matrix)):
 		# Send request and receive response into the array
 		try:
 			resp = urllib.request.urlopen(req)
-			useragent_matrix[j][2] = resp.read()
+			current_website_response = resp.read()
 		except:
 			print ("[ERROR] Could not request when connecting via " + proxylist_matrix[i][0] + " proxy")
 			continue
 
 		# Check whether current response is different from the control value
-		if useragent_matrix[j][2] != control_website_response:
+		if current_website_response != control_website_response:
 			
 			# Inform user that we've found one that is different from the control value
 			print ("[ALERT] DIFFERENCE detected when using geo-location " + proxylist_matrix[i][0] + " User-Agent: " + useragent_matrix[j][0])
 			print ("[ALERT] Details of the differences:")
 				
 			# Show the user the DIFF details
-			test_data_1 = useragent_matrix[0][2].splitlines()
-			test_data_2 = useragent_matrix[j][2].splitlines()
+			test_data_1 = control_website_response.splitlines()
+			test_data_2 = current_website_response.splitlines()
 			differ_instance = difflib.Differ()
 			diff_data = differ_instance.compare(test_data_1, test_data_2)
 			print('\n'.join(diff_data))
